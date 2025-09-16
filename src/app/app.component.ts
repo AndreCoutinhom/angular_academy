a333b5ee04ae402951662f018c0feeefd0629c81
Andre <andre.coutinhom@gmail.com>
2025-09-16 11:21:06 -0300

    Componente principal adicionado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { PrincipalComponent } from './principal/principal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CabecalhoComponent],
+  imports: [RouterOutlet, CabecalhoComponent, PrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
